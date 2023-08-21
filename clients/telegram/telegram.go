7fa3e1f35fc9c05e0a6fc8d0e437e1c31a0f1795
DSurfer <lolo80996@gmail.com>
2023-08-21 18:21:37 +0300

    making requests and handling errors

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,8 +1,10 @@
 package telegram
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"path"
 	"strconv"
 )
 
@@ -29,7 +31,27 @@ func (c *Client) Updates(offset int, limit int) ([]Update, error) {
 	q.Add("offset", strconv.Itoa(offset))
 	q.Add("limit", strconv.Itoa(limit))
 
-	//do request
+	//do request <- getUpdates
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("cant`t make request: %w", err)
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("can`t make request: %w", err)
+	}
 }
 
 func (c *Client) SendMessage() {
